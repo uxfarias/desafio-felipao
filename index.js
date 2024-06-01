a57d47e47471ea3fa4ccc1b24bae3a53f9aec77f
uxfarias <vulgofariaspessoal@gmail.com>
2024-06-01 16:31:23 -0300

    desafio concluído

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-console.log("Aqui já é o desafio")
-console.log("Essa é uma primeira alteração")
+const nome = "Vinicius"
+let exp = 0
+let rank = "Bronze"
+
+while (exp <= 10001){
+    exp += 1
+}
+
+if(exp <= 1001){
+    rank = "Bronze"
+}else if(exp > 1001 && exp <= 2001){
+    rank = "Prata"
+}else if(exp > 2001 && exp <= 3001){
+    rank = "Ouro"
+}else if(exp > 3001){
+    rank = "Diamante"
+}
+
+console.log("O " + nome + " está no nível " + rank + " com " + exp + " pontos de experiência")
